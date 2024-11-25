acb7136b090e302774537e0104310cbfc642a564
Hookiks <58189468+Hookiks@users.noreply.github.com>
2024-11-25 09:35:28 +0100

    Moteur
    Creation du moteur de jeu

---
+++ Gamelife2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <chrono>
+#include <utility>
+
+using namespace std;
+
+class Grille{
+    public:
+        int lignes;
+        int colonnes;
+        vector<vector<int>> cellules;
+        
+        bool VerifierCellulesVivante() {
+            for (int i = 0; i < lignes; ++i) {
+                for (int j = 0; j < colonnes; ++j) {
+                    if (cellules[i][j] == 1) {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+};
+
+
+class jeuDeLaVie{
+    private:
+        Grille grille;
+        
+};
