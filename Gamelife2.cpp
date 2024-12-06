d05a01662b2f7b7eb6ec0595298775e80a768fe5
Hookiks <58189468+Hookiks@users.noreply.github.com>
2024-12-06 10:00:42 +0100

    Modif encore

--- Gamelife2.cpp
+++ Gamelife2.cpp
@@ -3,7 +3,6 @@
 #include <thread>
 #include <chrono>
 #include <utility>
-#
 
 using namespace std; 
 
@@ -46,8 +45,8 @@ class jeuDeLaVie{
             system("cls"); //--> C'est CLS sur windows et sur mac c'est clear au cas ou Amin 
             for (auto& ligne : grille.cellules) {
                 for (auto& cellule : ligne) {
-                    // cout << (cellule ? "O" : " "); //--> O si vivant et " " si mort 
-                    cout << (cellule ? "1" : "0"); //--> 1 si vivant et 0 si mort
+                    cout << (cellule ? "O" : " "); //--> O si vivant et " " si mort 
+                    // cout << (cellule ? "1" : "0"); //--> 1 si vivant et 0 si mort
                 }
                 cout << endl;
             }
